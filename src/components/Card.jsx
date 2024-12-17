677516e2622fd86a1c11d7df7e803c052f648ea2
Laassel <lassel.giovanni@gmail.com>
2024-12-18 00:11:03 +0100

    ADD fontawesome and  function that transforms the number from 1 to 5

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
 import React from "react"
-import { getFlagUrl } from "../flags/flagMapper";
-import styles from "./Card.module.css"; 
+import { getFlagUrl } from "../flags/flagMapper"
+import styles from "./Card.module.css" 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons"
+import {faStar as regularStar} from "@fortawesome/free-regular-svg-icons"
 
 export default function Card ({ movie }) {
-  console.log("Dati ricevuti nel componente Card:", movie);
-  console.log(movie.vote_avarege)
+  // console.log("Dati ricevuti nel componente Card:", movie);
+  // console.log(movie.vote_avarege)
+  // funzione per traformare il voto in stelle 
+  const getStars = (rating) => {
+    const starsCount = Math.ceil(rating / 2)
+    const fullStars = Array(starsCount).fill(1)     // stelle piene
+    const emptyStars = Array(5 - starsCount).fill(0) // stelle vuote
+    return [...fullStars, ...emptyStars];
+  }
  
     return (
       
@@ -31,7 +41,14 @@ export default function Card ({ movie }) {
 
             </p>
             <p className="card-text">
-              <strong>Rating:</strong> {movie.rating}
+              <strong>Rating:</strong> {""}
+              {getStars(movie.rating).map((star, index) => (
+                <FontAwesomeIcon
+                  key={index}
+                  icon={star ? solidStar : regularStar} // Stella piena o vuota
+                  style={{ color: "#FFD700", marginRight: "4px" }} 
+                />
+              ))}
             </p>
             <p className="card-text">
               <strong>Type:</strong> {movie.type === "movie" ? "Film" : "Serie TV"}
